a38f00949f5a2b06803ba96e16cf18454ee3da1e
LudoRNLT <102245368+LudoRNLT@users.noreply.github.com>
2023-11-21 14:15:59 +0100

    Update Bastien.js
    Correction frames ki

--- Js/fighters/Bastien.js
+++ Js/fighters/Bastien.js
@@ -1,160 +1,26 @@
 import { Fighter } from './Fighter.js';
 
 export class Bastien extends Fighter {
+    /*
     handleKey(event) {
-        switch (event.key) {
-            case "ArrowLeft":
-                this.moveLeft();
-                break;
-            case "ArrowRight":
-                this.moveRight();
-                break;
-            case " ":
-                this.jump();
-                break;
-            case "g":
-            case "G":
-                this.punch();
-                this.punchPressed = true;
-                this.checkUpKick();
-                this.checkLowKick();
-                break;
-            case "ArrowUp":
-                this.upPressed = true;
-                this.checkUpKick();
-                break;
-            case "h":
-            case "H":
-                this.hadoken();
-                break;
-            case "ArrowDown":
-                this.downPressed = true;
-                this.guard();
-                this.checkLowKick();
-                break;
-            case "k":
-            case "K":
-                this.ki();
-                break;
-
-        }
-    }
-
-    handleKeyup(event) {
-        switch (event.key) {
-            case "ArrowLeft":
-            case "ArrowRight":
-                this.stopMovement();
-                break;
-            case "g":
-            case "G":
-                this.punchPressed = false;
-                break;
-            case "ArrowUp":
-                this.upPressed = false;
-                break;
-            case "h":
-            case "H":
-                break;
-            case "ArrowDown":
-                this.downPressed = false;
-                this.stopGuard();
-                break;
-            case " ":
-                this.jump();
-                break;
-            case "k":
-            case "K":
-                this.ki();
-                break;
-        }
-    }
-
-    moveLeft() {
-        this.velocity = -100;
-        this.changeState('walk');
-        this.regardeDroite = false;
-    }
-
-    moveRight() {
-        this.velocity = 100;
-        this.changeState('walk');
-        this.regardeDroite = true;
-    }
-
-    jump() {
-        if (!this.jumping) {
+        if (event.key === "ArrowLeft") {
+            this.animationFrame++;
+            console.log(this.animationFrame);
+        } else if (event.key === "ArrowRight") {
+            this.animationFrame--;
+            console.log(this.animationFrame);
+        } else if (event.key === "ArrowUp" && !this.jumping) {
             this.jumping = true;
             this.velocityY = this.jumpSpeed;
-            this.changeState('jump');
+            this.state = 'jump';
         }
-    }
-
-    punch() {
-        this.isPunching = true;
-        this.changeState('punch');
-    }
-
-    hadoken() {
-        this.isHadoken = true;
-        this.changeState('hadoken');
-    }
-
-    stopMovement() {
-        this.velocity = 0;
-        if (!this.jumping) {
-            this.changeState('idle');
-        }
-    }
-    guard() {
-        this.changeState('guard');
-    }
-
-    stopGuard() {
-        this.changeState('idle');
-    }
-
-    upKick() {
-        this.changeState('up_kick');
-    }
-    checkUpKick() {
-        if (this.upPressed && this.punchPressed) {
-            this.upKick();
-        }
-    }
-
-    lowKick() {
-        this.changeState('low_kick');
-    }
-
-    checkLowKick() {
-        if (this.downPressed && this.punchPressed) {
-            this.lowKick();
-        }
-    }
-
-    ki() {
-        this.isKi = true;
-        this.changeState('ki');
-    }
-
+    }*/
 
     constructor(x, y, velocity) {
         super('Bastien', x, y, velocity);
         this.image = document.body.querySelector('img[alt="bastien"]');
         this.frames = new Map();
-        this.regardeDroite = true;
-        this.jumping = false;
-        this.velocityY = 0;
-        this.jumpSpeed = -5;
-        this.gravity = 0.25;
-        this.entryPlayed = false;
-        this.state = 'entry';
-        this.upPressed = false;
-        this.punchPressed = false;
-        this.downPressed = false;
-
-
+        this.state = "ki";
 
         let walk = [
             [10, 174, 35, 64],
@@ -326,55 +192,43 @@ export class Bastien extends Fighter {
             [606, 2460, 35, 68],
             [655, 2463, 35, 67]
         ];
+        
+        this.frames = new Map([
+            ['ki-1', [[10,1905,35,67] , [18, 67]]],
+            ['ki-2', [[53,1908,34,64] , [18, 64]]],
+            ['ki-3', [[98,1903,39,68] , [18, 68]]],
+            ['ki-4', [[144,1899,53,70] , [27, 69]]],
+            ['ki-5', [[209,1902,43,66] , [22, 66]]],
+            ['ki-6', [[263,1898,53,69] , [27, 69]]],
+            ['ki-7', [[324,1897,53,74] , [27, 69]]],
+            ['ki-8', [[389,1883,53,89] , [27, 84]]],
+            ['ki-9', [[459,1902,53,75] , [27, 70]]],
+            ['ki-10', [[520,1899,53,78] , [27, 71]]],
+            ['ki-11', [[576,1879,53,100] , [27, 93]]],
+            ['ki-12', [[0,2003,101,79] , [54, 71]]],
+            ['ki-13', [[106,1992,107,91] , [55, 83]]],
+            ['ki-14', [[215,1979,127,106] , [67, 97]]],
+            ['ki-15', [[347,1995,127,92] , [67, 83]]],
+            ['ki-16', [[484,1990,115,101] , [55, 92]]],
+            ['ki-17', [[5,2096,126,105] , [66, 96]]],
+            ['ki-18', [[144,2103,126,101] , [66, 92]]],
+            ['ki-19', [[284,2103,117,101] , [57, 92]]],
+            ['ki-20', [[410,2098,117,106] , [57, 97]]],
+            ['ki-21', [[538,2103,115,101] , [55, 92]]],
+            ['ki-22', [[5,2235,96,101] , [49, 92]]],
+            ['ki-23', [[111,2212,96,124] , [49, 115]]],
+            ['ki-24', [[220,2256,64,78] , [34, 71]]],
+            ['ki-25', [[303,2262,34,67] , [18, 67]]],
+        ]);
 
-        // this.frames = [
-        //     ['ki-1', [[10,1905,35,67] , [17.5, 67]]],
-        //     ['ki-2', [[53,1908,34,64] , [17, 64]]],
-        //     ['ki-3', [[98,1903,39,68] , [19.5, 68]]],
-        //     ['ki-4', [[144,1899,53,69] , [26.5, 69]]],
-        //     ['ki-5', [[209,1902,43,66] , 21.5, 66]]],
-        //     ['ki-6', [[262,1898,53,70] , [26.5, 68]]],
-        //     ['ki-7', [[324,1897,53,74] , [26.5, 69]]],
-        //     ['ki-8', [[389,1883,53,89] , [26.5, 84]]],
-        //     ['ki-9', [[459,1902,53,75] , [26.5, 70]]],
-        //     ['ki-10', [[520,1899,53,78] , [26.5, 71]]],
-        //     ['ki-11', [[576,1879,53,100] , [26.5, 93]]],
-        //     ['ki-12', [[5,2003,101,79] , [26.5, 71]]],
-        //     ['ki-13', [[112,1992,101,91] , [26.5, 68]]],
-        //     ['ki-14', [[215,1979,127,106] , [26.5, 97]]],
-        //     ['ki-15', [[347,1995,127,92] , [26.5, 70]]],
-        //     ['ki-16', [[484,1990,115,101] , [26.5, 71]]],
-        //     ['ki-17', [[5,2096,126,105] , [26.5, 96]]],
-        //     ['ki-18', [[144,2103,126,101] , [26.5, 69]]],
-        //     ['ki-19', [[284,2103,117,101] , [26.5, 69]]],
-        //     ['ki-20', [[410,2098,117,106] , [26.5, 97]]],
-        //     ['ki-21', [[538,2103,115,101] , [26.5, 70]]],
-        //     ['ki-22', [[5,2235,96,101] , [26.5, 71]]],
-        //     ['ki-23', [[111,2212,96,124] , [26.5, 115]]],
-        //     ['ki-24', [[220,2256,64,78] , [26.5, 71]]],
-        //     ['ki-25', [[303,2262,34,67] , [17, 67]]],
-        //     ['hit-1', [[23,2364,41,64] , [20.5, 64]]],
-        //     ['dead-1', [[6,2448,45,81] , [22.5, 81]]],
-        //     ['dead-2', [[62,2464,65,61] , [32.5, 61]]],
-        //     ['dead-3', [[130,2500,68,27] , [34, 27]]],
-        //     ['dead-4', [[204,2482,65,44] , [32.5, 44]]],
-        //     ['dead-5', [[281,2499,66,22] , [33, 22]]],
-        //     ['dead-6', [[357,2497,50,26] , [25, 26]]],
-        //     ['dead-7', [[417,2473,34,51] , [17, 51]]],
-        //     ['dead-8', [[459,2460,34,67] , [17, 67]]],
-        //     ['dead-9', [[505,2467,43,61] , [21.5, 61]]],
-        //     ['dead-10', [[548,2474,51,53] , [25.5, 53]]],
-        //     ['dead-11', [[606,2460,35,68] , [17.5, 68]]],
-        //     ['dead-12', [[655,2463,35,67] , [17.5, 67]]],
-        //     ['walk-1', [[10,174,35,64] , [17.5, 64]]],
-        //     ['walk-2', [[54,171,37,67] , [18.5, 67]]],
-        //     ['walk-3', [[100,172,35,68] , [17.5, 68]]],
-        //     ['walk-4', [[141,172,35,68] , [17.5, 68]]],
-        //     ['walk-5', [[180,172,35,68] , [17.5, 68]]],
-        //     ['walk-6', [[
-        // ];
+        this.animation = {
+            'ki': ['ki-1','ki-2','ki-3','ki-4','ki-5','ki-6','ki-7','ki-8','ki-9','ki-10',
+                'ki-11','ki-12','ki-13','ki-14','ki-15','ki-16','ki-17','ki-18','ki-19','ki-20',
+                'ki-21','ki-22','ki-23','ki-24','ki-25',
+            ],
+        };
 
-        this.frames = this.gen_map("ki-", ki);
+        // this.frames = this.gen_map("ki-", ki);
         this.frames = this.gen_map("hit-", hit);
         this.frames = this.gen_map("dead-", dead);
         this.frames = this.gen_map("walk-", walk);
@@ -393,7 +247,7 @@ export class Bastien extends Fighter {
         this.frames = this.gen_map("crouch_uppercut-", crouchUppercut);
         this.frames = this.gen_map("crouch_spinkick-", crouchSpinkick);
         
-        this.animation = this.gen_AnimationObject("ki", "ki", 25);
+        // this.animation = this.gen_AnimationObject("ki", "ki", 25);
         this.animation = this.gen_AnimationObject("hit", "hit", 1);
         this.animation = this.gen_AnimationObject("walk", "walk", 6);
         this.animation = this.gen_AnimationObject("jump", "jump", 8);
